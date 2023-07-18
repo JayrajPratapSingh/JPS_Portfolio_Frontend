6bbd3fee0138ccd3175c7778aff8de92bdbf8d3b
JayrajPratapSingh <jairaj273310@gmail.com>
2023-07-18 13:15:51 +0530

    changes in package.json

--- src/index.js
+++ src/index.js
@@ -1,24 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
-import {Provider} from "react-redux"
-import store from './Store';
-import {Provider as AlertProvider, positions,transitions} from "react-alert"
-import AlertTemplate from "react-alert-template-basic"
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { Provider } from "react-redux";
+import store from "./Store";
+import { Provider as AlertProvider, positions, transitions } from "react-alert";
+import AlertTemplate from "react-alert-template-basic";
 
 const options = {
-  positions:positions.BOTTOM_CENTER,
+  position: positions.BOTTOM_CENTER,
   timeout: 5000,
-  transitions:transitions.SCALE,
-}
-root.render(
+  transition: transitions.SCALE,
+};
+
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-    <AlertProvider template={AlertTemplate} {...options}>
-    <App />
-    </AlertProvider>
+      <AlertProvider template={AlertTemplate} {...options}>
+        <App />
+      </AlertProvider>
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
